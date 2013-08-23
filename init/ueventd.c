a0af35316bcad59fa6b78d366e8cf1effbdb2e9b
Ricardo Salveti de Araujo <ricardo.salveti@canonical.com>
2013-08-23 01:52:10 -0300

    init/ueventd.c: terminating ueventd in case it receives POLLERR
    Due race with udev, poll can end up receiving POLLERR, which wasn't
    handled in the original code, causing an infinite loop. This patch just
    change that behavior to terminate ueventd, so init can restart it
    properly to be able to receieve and handle new events (it'll skip
    coldboot if already done at least once).
    
    We can't yet disable ueventd because it's needed for proper firmware and
    device handling (usb gadget, bluetooth, wlan, etc).
    
    This at least unblocks bug http://pad.lv/1190792
    
    Change-Id: Icd4404dc98e42f3dd9ba91038b978c8c43e4347d
    Signed-off-by: Ricardo Salveti de Araujo <ricardo.salveti@canonical.com>
    Signed-off-by: Sergio Schvezov <sergio.schvezov@canonical.com>

--- init/ueventd.c
+++ init/ueventd.c
@@ -99,6 +99,10 @@ int ueventd_main(int argc, char **argv)
         nr = poll(&ufd, 1, -1);
         if (nr <= 0)
             continue;
+        if (ufd.revents & POLLERR) {
+               ERROR("got POLLERR, terminating ueventd\n");
+               exit(1);
+        }
         if (ufd.revents & POLLIN)
                handle_device_fd();
     }
